175f65a44ca19ad6b36b70f5d018e124fe9bbe85
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-03 00:21:45 +0100

    Finishing The Data Access And Business Layers ANd Create Two APIs (Cet All - Get By ID)

--- clsStudentsClassesData.cs
+++ clsStudentsClassesData.cs
@@ -1,12 +1,217 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace SchoolAPiDataAccessLayer
 {
-    internal class clsStudentsClassesData
+    public record studentClassDTO(int ID, int StudentID, int ClassID);
+
+    public class clsStudentsClassesData
     {
+        public static async Task<IEnumerable<studentClassDTO>> GetAllAsync()
+        {
+            var StudentClasses = new List<studentClassDTO>();
+
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("sp_studentClasses_FindAll", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                StudentClasses.Add(new studentClassDTO
+                                    (
+                                         reader.GetInt32(reader.GetOrdinal("StudentClassID")),
+                                                  reader.GetInt32(reader.GetOrdinal("StudentID")),
+                                                  reader.GetInt32(reader.GetOrdinal("ClassID"))
+
+                                    ));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return StudentClasses;
+        }
+
+        public static async Task<studentClassDTO> GetByIdAsync(int ID)
+        {
+            studentClassDTO studentClass = null;
+
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("sp_studentClasses_FindByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@ID", ID);
+
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                studentClass = new studentClassDTO
+                                             (
+                                                  reader.GetInt32(reader.GetOrdinal("StudentClassID")),
+                                                  reader.GetInt32(reader.GetOrdinal("StudentID")),
+                                                  reader.GetInt32(reader.GetOrdinal("ClassID"))
+                                             );
+                            };
+
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return studentClass;
+        }
+
+
+
+        public static async Task<int> AddAsync(studentClassDTO studentClass)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    using (var command = new SqlCommand("sp_studentClasses_Insert", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@ID", studentClass.StudentID);
+                        command.Parameters.AddWithValue("@CLassID", studentClass.ClassID);
+
+                        var outputIdParam = new SqlParameter("@NewID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+
+                        command.Parameters.Add(outputIdParam);
+
+                        await connection.OpenAsync();
+                        await command.ExecuteNonQueryAsync();
+
+                        return (int)outputIdParam.Value;
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static async Task<bool> UpdateAsync(studentClassDTO studentClass)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("sp_studentClasses_Update", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+
+                        command.Parameters.AddWithValue("@ID", studentClass.ID);
+                        command.Parameters.AddWithValue("@ID", studentClass.StudentID);
+                        command.Parameters.AddWithValue("@CLassID", studentClass.ClassID);
+
+                        return await command.ExecuteNonQueryAsync() > 0;
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static async Task<bool> DeleteAsync(int ID)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    using (var command = new SqlCommand("sp_studentClasses_Delete", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@ID", ID);
+
+                        await connection.OpenAsync();
+                        return await command.ExecuteNonQueryAsync() > 0;
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static async Task<bool> IsExistsAsync(int ID)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+
+                    using (var command = new SqlCommand("sp_studentClasses_Exists", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@ID", ID);
+
+                        await connection.OpenAsync();
+                        var result = await command.ExecuteScalarAsync();
+                        return Convert.ToBoolean(result);
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
