8843fcb2bb539951f598fa686741cfae9671efc6
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-08 15:44:41 +0100

    Add Get All And Get By ID In The Three Layers ✅ Test The Methods With Swagger : Result --> Success ✅

--- clsEmailsData.cs
+++ clsEmailsData.cs
@@ -1,6 +1,96 @@
-﻿namespace SchoolAPiDataAccessLayer
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SchoolAPiDataAccessLayer
 {
+    public record EmailDTO(int ID, string Address, int PersonID, int EmailTypeID, bool IsPrimary);
     public class clsEmailsData
     {
+        public static async Task<IEnumerable<EmailDTO>> GetAllAsync()
+        {
+            var emails = new List<EmailDTO>();
+
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("sp_emails_FindAll", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                emails.Add(new EmailDTO
+                                    (
+                                        reader.GetInt32(reader.GetOrdinal("EmailID")),
+                                        reader.GetString(reader.GetOrdinal("EmailAddress")),
+                                        reader.GetInt32(reader.GetOrdinal("PersonID")),
+                                        reader.GetInt32(reader.GetOrdinal("EmailTypeID")),
+                                        reader.GetBoolean(reader.GetOrdinal("IsPrimary"))
+                                    ));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return emails;
+        }
+
+        public static async Task<EmailDTO> GetByIdAsync(int EmailID)
+        {
+            EmailDTO email = null;
+
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("sp_emails_FindByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@EmailID", EmailID);
+
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                email = new EmailDTO
+                                             (
+                                                reader.GetInt32(reader.GetOrdinal("EmailID")),
+                                        reader.GetString(reader.GetOrdinal("EmailAddress")),
+                                        reader.GetInt32(reader.GetOrdinal("PersonID")),
+                                        reader.GetInt32(reader.GetOrdinal("EmailTypeID")),
+                                        reader.GetBoolean(reader.GetOrdinal("IsPrimary"))
+                                              );
+                            };
+
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return email;
+        }
+
+
     }
 }
