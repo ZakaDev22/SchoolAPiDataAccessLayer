cfc8fdee08e852282c93347a07d95cd179b0364c
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-28 21:19:47 +0100

    Add relationships Types ( Get All And Get By ID) In Three Layers

--- clsRelationshipTypesData.cs
+++ clsRelationshipTypesData.cs
@@ -1,12 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace SchoolAPiDataAccessLayer
 {
-    internal class clsRelationshipTypesData
+    public record RelationshipTypeDTO(int ID, string Name);
+    public class clsRelationshipTypesData
     {
+        public static async Task<IEnumerable<RelationshipTypeDTO>> GetAllAsync()
+        {
+            var relationshipType = new List<RelationshipTypeDTO>();
+
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("sp_relationshiptypes_FindAll", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                relationshipType.Add(new RelationshipTypeDTO
+                                    (
+                                        reader.GetInt32(reader.GetOrdinal("RelationshipTypeID")),
+                                        reader.GetString(reader.GetOrdinal("RelationshipTypeName"))
+                                    ));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return relationshipType;
+        }
+
+
+
+        public static async Task<RelationshipTypeDTO> GetByIdAsync(int ID)
+        {
+            RelationshipTypeDTO relationshipType = null;
+
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("sp_relationshiptypes_FindByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@RelationshipTypeID", ID);
+
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                relationshipType = new RelationshipTypeDTO
+                                             (
+                                               reader.GetInt32(reader.GetOrdinal("RelationshipTypeID")),
+                                                 reader.GetString(reader.GetOrdinal("RelationshipTypeName"))
+                                              );
+                            };
+
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return relationshipType;
+        }
     }
 }
