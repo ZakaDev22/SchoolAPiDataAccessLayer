a2e954033a289479138f911538849b9fbafc6389
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-29 22:14:29 +0100

    Add Address Delete Method In The Three Layers

--- clsAddressesData.cs
+++ clsAddressesData.cs
@@ -136,5 +136,32 @@ namespace SchoolAPiDataAccessLayer
 
             return addresses;
         }
+
+        public static async Task<bool> DeleteAsync(int ID)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(DataGlobal._connectionString))
+                {
+                    using (var command = new SqlCommand("sp_addresses_Delete", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@AddressID", ID);
+
+                        await connection.OpenAsync();
+                        return await command.ExecuteNonQueryAsync() > 0;
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
